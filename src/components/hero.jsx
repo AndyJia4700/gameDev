3f33093f111eb736a61c904bd57a2c98999ce06a
andy <jia470079834@gmail.com>
2021-02-05 21:26:27 -0500

    css

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -14,6 +14,7 @@ class Hero extends React.Component{
         const speed = 2;
         const heldDirections = [];
         const pixelSize = parseInt(
+            
             getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
         )
  
@@ -23,7 +24,7 @@ class Hero extends React.Component{
 
 
 
-            
+
 
         }
         
